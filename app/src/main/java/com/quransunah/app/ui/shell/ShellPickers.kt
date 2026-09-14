0d3386c01951228b7d4ec92c1ab61ffcf833e458
msalbarho <2.06375244e+08+msalbarho@users.noreply.github.com>
2026-09-14 05:54:41 +0000

    Fix training control arrangement import

--- app/src/main/java/com/quransunah/app/ui/shell/ShellPickers.kt
+++ app/src/main/java/com/quransunah/app/ui/shell/ShellPickers.kt
@@ -9,6 +9,7 @@ import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
